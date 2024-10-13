2d77aaa2cdbc20dafbaa07103fd1ddbd5a27bb0d
Trinhcanh2712 <trinhminhcanh27122004@gmail.com>
2024-10-13 22:03:32 +0700

    Update union.c

--- STRUCT - UNION/union.c
+++ STRUCT - UNION/union.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
+#include <stdint.h> // Thư viện chứa uint32_t
+
+// Khai báo struct có các thành viên là mảng và uint32_t
+union MyUnion {
+    uint32_t arr1[5];   // 
+    uint16_t arr2[3];   // 
+    uint64_t arr3[2];   // 8 + 8
+};
+
+int main() {
+    printf("Size of union MyUnion: %lu bytes\n", sizeof(Union MyStruct));
+    return 0;
+}
+
 
